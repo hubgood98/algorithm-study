3175478339d1ac99cf6734f613e675fe3100b807
hubgood98 <heej1998@gmail.com>
2024-08-16 01:34:20 +0900

    내용추가

---
+++ algorithm/src/main/java/org/study/AntArmy.java
@@ -0,0 +1,16 @@
+package src.main.java.org.study;
+
+public class AntArmy {
+
+    public static int solution(int hp) {
+
+        return hp / 5 + (hp % 5)/3 + (hp % 5 % 3);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(23));
+        System.out.println(solution(24));
+        System.out.println(solution(999));
+    }
+
+}
