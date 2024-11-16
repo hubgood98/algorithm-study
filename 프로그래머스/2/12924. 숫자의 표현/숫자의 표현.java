c87fc9a1bd1ad83f5560c11443320c85df36db2b
Heejun Kim <127611039+hubgood98@users.noreply.github.com>
2024-11-16 19:54:51 +0900

    [level 2] Title: 숫자의 표현, Time: 0.12 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -4,8 +4,8 @@ class Solution {
          int count = 0;
         
         // k는 연속된 자연수의 개수 (1부터 n까지)
-        for (int k = 1; k * (k + 1) / 2 <= n; k++) {
-            if ((n - (k * (k - 1)) / 2) % k == 0) {
+        for (int k = 1; k <= n; k+=2) {
+            if (n % k == 0) {
                 count++;
             }
         }
